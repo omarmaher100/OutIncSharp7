105f7a8ae37f40f7c6c6730db22bb66a3699cb33
omar maher <omarmaher100@gmail.com>
2017-04-23 14:59:47 +0300

    Add project

---
+++ OutIncSharp7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutIncSharp7
+{
+    class Program
+    {
+        public static void MyName(out string firstName, out string lastName)
+        {
+            firstName = "Omar";
+            lastName = "Maher";
+        }
+        public static void OldWay()
+        {
+            string firstName;
+            string lastName;
+            MyName(out firstName, out lastName);
+
+            Console.WriteLine($"My Name is :{ firstName} {lastName}  => Old way");
+        }
+
+        public static void CSharp7Way()
+        {
+            MyName(out string firstName, out string lastName);
+
+            Console.WriteLine($"My Name is :{ firstName} {lastName}  => c#7 way");
+        }
+        static void Main(string[] args)
+        {
+            OldWay();
+            CSharp7Way();
+        }
+    }
+}
